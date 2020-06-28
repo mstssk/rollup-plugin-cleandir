ed891df556bced6a8415c604c7d45af4a3892129
mstssk <mstssk+github@gmail.com>
2020-06-29 02:14:35 +0900

    Parameters are required at least 1.

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import { Plugin } from "rollup";
 import { cleandir as _cleandir } from "@mstssk/cleandir";
 
-export function cleandir(...paths: string[]): Plugin {
+export function cleandir(path: string, ...paths: string[]): Plugin {
   return {
     name: "cleandir",
     async buildStart() {
-      await _cleandir(paths);
+      await _cleandir([path, ...paths]);
     },
   };
 }
