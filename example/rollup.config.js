f55c408260e03897a8c29cdc5f489afed2077b4a
mstssk <mstssk+github@gmail.com>
2020-06-29 02:05:12 +0900

    example

---
+++ example/rollup.config.js
@@ -0,0 +1,12 @@
+import { cleandir } from "rollup-plugin-cleandir";
+
+const OUT_DIR = "./dist";
+
+export default {
+  input: ["index.js"],
+  output: {
+    dir: OUT_DIR,
+    format: "es",
+  },
+  plugins: [cleandir(OUT_DIR)],
+};
