fee34fd51425e0545f9ef08daa5465cf9409dd3b
mstssk <mstssk+github@gmail.com>
2020-06-30 01:10:06 +0900

    Improve parameter type.

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import { Plugin } from "rollup";
 import { cleandir as _cleandir } from "@mstssk/cleandir";
 
-export function cleandir(path: string, ...paths: string[]): Plugin {
+export function cleandir(dirs: string | string[]): Plugin {
   return {
     name: "cleandir",
-    async buildStart() {
-      await _cleandir([path, ...paths]);
+    buildStart() {
+      return _cleandir(dirs);
     },
   };
 }
