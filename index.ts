5088ecf966ef81e0f07d02ea57fd87c1fe0d5c23
mstssk <mstssk+github@gmail.com>
2022-01-09 02:25:38 +0900

    Clean outputOptions.dir automaticaly.

--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
-import { Plugin } from "rollup";
+import type { Plugin } from "rollup";
 import { cleandir as _cleandir } from "@mstssk/cleandir";
 
-export function cleandir(dirs: string | string[]): Plugin {
+/**
+ * Clean output directory.
+ * @param dirs [Optional]
+ * @returns {Plugin} Rollup Plugin
+ */
+export function cleandir(dirs?: string | string[]): Plugin {
+  const useOutputOptionsDir = dirs == null;
   return {
     name: "cleandir",
     buildStart() {
-      return _cleandir(dirs);
+      if (!useOutputOptionsDir) {
+        return _cleandir(dirs);
+      }
+    },
+    renderStart(outputOptions) {
+      if (useOutputOptionsDir && outputOptions.dir) {
+        return _cleandir(outputOptions.dir);
+      }
     },
   };
 }
